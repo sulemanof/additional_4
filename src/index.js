dc33a50aa2ff7441cc34fc386254279267213c72
Your Namesulemanof <suleimandaniil@gmail.com>
2017-09-10 14:21:08 +0300

    final solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    var num1 = first.split('').reverse();
+    var num2 = second.split('').reverse();
+    var res = [];
+
+    for (var i = 0; num1[i] >= 0; i++) {
+        for (var j = 0; num2[j] >= 0; j++) {
+
+            if (!res[i + j]) res[i + j] = 0;
+            res[i + j] += (num1[i] * num2[j]);
+        }
+    }
+    for (var k = 0; res[k] >= 0; k++) {
+        if (res[k] >= 10) {
+            if (!res[k + 1]) res[k + 1] = 0;
+            res[k + 1] += parseInt(res[k] / 10);
+            res[k] = res[k] % 10;
+        }
+    }
+    var solution = res.reverse().join('');
+    return solution;
 }
